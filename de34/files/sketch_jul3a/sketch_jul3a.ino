f64e1e8fa98d635fca5446711931bd3c17689b6a
ryu347 <r202201831uy@jindai.jp>
2023-07-17 02:25:01 +0900

    1

--- de34/files/sketch_jul3a/sketch_jul3a.ino
+++ de34/files/sketch_jul3a/sketch_jul3a.ino
@@ -1,38 +1,22 @@
 const int DIR  = 8;
 const int STEP = 9;
-int val=0;
-int from_pc='0';
-
 
 void setup() {
   pinMode(DIR, OUTPUT);
   pinMode(STEP, OUTPUT);
   digitalWrite(DIR, LOW);
   digitalWrite(STEP, LOW);
-
-  pinMode(13,OUTPUT);
-  Serial.begin(9600);  
 }
 
 void loop() {
 
-  if(Serial.available() < 0){
-    from_pc = Serial.read();
-}
-
-  if (from_pc=='1'){
-    for (int i=0; i <= 3000; i++){
-      clockwise(5000);
-    }
-    for (int i=0; i <= 50; i++){
-      counter_clockwise(3000);
-    }
-  }else if (from_pc=='0'){
-
-  }    
-
-}
-
+  for (int i=0; i <= 3000; i++){
+    clockwise(5000);
+  }
+  for (int i=0; i <= 100; i++){
+    counter_clockwise(5000);
+  }
+}    
 
 void clockwise(int delaytime){
   digitalWrite(DIR, HIGH);//HIGHは時計回り
